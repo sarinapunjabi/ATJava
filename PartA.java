92e72d7e8fbb522cb947645742a76069f1cc7bbe
sarinapunjabi <157131469+sarinapunjabi@users.noreply.github.com>
2025-11-12 02:37:24 -0500

    Add files via upload

--- PartA.java
+++ PartA.java
@@ -1,28 +1,14 @@
-// I had to look up how to convert from F to C
-import java.util.Scanner;
-
 public class PartA {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        double material = 100.0; 
+        int years = 0;   
 
-        System.out.print("temperature: ");
-        double temp = input.nextDouble();
-
-        System.out.print("Celsius (C) or Fahrenheit (F): ");
-        String scale = input.next().toUpperCase();
-
-        if (scale.equals("F")){
-            temp = ((temp - 32) * 5) / 9;
+        while (material > 1.0){ 
+            material = material / 2; 
+            years = years + 30;        
         }
-
-        if (temp <= 0){
-            System.out.println("water is ice (solid)");
-        } else if (temp >= 100){
-            System.out.println("water is steam (gas)");
-        } else {
-            System.out.println("water is in liquid state");
-        }
-
-        input.close();
+        System.out.println("For Cesium 90:");
+        System.out.println("Years required to decay: " + years);
+        System.out.println("Material left: " + material + "%");
     }
 }
--- PartB.java
+++ PartB.java
@@ -3,14 +3,17 @@ import java.util.Scanner;
 public class PartB {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter two floating point numbers: ");
-        double num1 = input.nextDouble();
-        double num2 = input.nextDouble();
+        System.out.print("Enter the annual interest rate (APR) percentage: ");
+        double rate = input.nextDouble();
+        System.out.print("Enter the initial balance: ");
+        double balance = input.nextDouble();
+        System.out.println();
+        System.out.println("Year Interest Balance");
 
-        if (Math.abs(num1 - num2) < 0.01){
-            System.out.println("They are the same up to two decimal places.");
-        } else{
-            System.out.println("They are different.");
+        for (int year = 1; year <= 5; year++){
+            double interest = balance * (rate / 100);
+            balance = balance + interest;
+            System.out.printf("%d %.2f %.2f%n", year, interest, balance);
         }
 
         input.close();
